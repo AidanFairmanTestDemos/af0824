2ef279a349c76aef60d3c735fe22aa20f7d9b137
Aidan Fairman <aidan@Aidans-MacBook-Pro.local>
2022-08-26 17:17:37 -0400

    initial gradle and spring are set up

---
+++ service/src/test/java/fairman/aidan/tool_rental/ToolRentalApplicationTests.java
@@ -0,0 +1,13 @@
+package fairman.aidan.tool_rental;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class ToolRentalApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
